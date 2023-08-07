0bafeaa21361a484f11c81db7d62654f0b76fb26
vicohadiansyah <hadiansyahvico@gmail.com>
2023-08-07 20:20:07 +0700

    first commit

---
+++ Test/secondtest.js
@@ -0,0 +1,70 @@
+const {Builder, By, Key} = require ("selenium-webdriver");
+var should = require("chai").should();
+
+//describe  block
+describe("add anothet todo tests", function(){
+    
+    //it block
+    it("succses fully add another on todo list", async function(){
+
+        //lauch brwoser 
+        let driver = await new Builder().forBrowser("chrome").build();
+
+        //navigate url 
+        await driver.get("https://lambdatest.github.io/sample-todo-app/")
+
+        // add a todo
+        await driver.findElement(By.id("sampletodotext")).sendKeys("Selenium Pratice", Key.RETURN);
+
+        //assertion
+        let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+            return value
+        });
+
+        //assert use chai should
+        todoText.should.equal("Selenium Pratice")
+
+        //close browser
+        await driver.quit();
+    }); 
+
+    it("Add new test for report", async function(){
+
+        //lauch brwoser 
+        let driver = await new Builder().forBrowser("chrome").build();
+
+        //navigate url 
+        await driver.get("https://lambdatest.github.io/sample-todo-app/")
+
+        // add a todo
+        await driver.findElement(By.id("sampletodotext")).sendKeys("Selenium Pratice", Key.RETURN);
+
+        //assertion
+        let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+            return value
+        });
+
+        //assert use chai should
+        todoText.should.equal("Selenium ")
+
+        //close browser
+        await driver.quit();
+    }); 
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
